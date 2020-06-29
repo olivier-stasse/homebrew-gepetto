41bea3cd1b5b4f715f7b1be593df405dbf69758b
Olivier Stasse <ostasse@laas.fr>
2020-06-29 22:19:00 +0200

    [dynamic-graph] Fix message and bottle SHA256

--- Formula/dynamic-graph.rb
+++ Formula/dynamic-graph.rb
@@ -1,14 +1,14 @@
 class DynamicGraph < Formula
-  desc "Dynamic Graph"
+  desc "Stack-Of-Tasks Dynamic Graph"
   homepage "https://github.com/stack-of-tasks/dynamic-graph"
   url "https://github.com/stack-of-tasks/dynamic-graph/releases/download/v4.2.1/dynamic-graph-v3-4.2.1.tar.gz"
   sha256 "807565a836963d1f05ca8acf16366760b3c5c9a38bb60d8e75c422419747ea89"
   head "https://github.com/stack-of-tasks/dynamic-graph.git", :branch => "devel"
 
-#  bottle do
-#    root_url "https://github.com/stack-of-tasks/dynamic-graph/releases/download/v4.2.1"
-#    sha256 "9e7db5a4c8d133ad24cdda67ac5e19d94932fcdd9795cd308097259f04bffc40" => :mojave
-#  end
+  bottle do
+    root_url "https://github.com/stack-of-tasks/dynamic-graph/releases/download/v4.2.1"
+    sha256 "4c4ddf4a62fcaac66d5c3d49c2cd81b231ee59299ed21697cbd5ed9024057ac6" => :catalina
+  end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
